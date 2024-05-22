88c7a42e59df7bee10c178fc7fb3ca74b7ffa293
Diego Rojas <darb302@gmail.com>
2024-05-22 14:47:08 -0300

    create stg models

---
+++ src/models/staging/stg_bcra_main_indicators.sql
@@ -0,0 +1,20 @@
+with source as ( 
+
+    select * from {{ source('raw_bcra_api', 'raw_bcra_api') }}
+
+),
+
+renamed as (
+    
+    select 
+
+        "idVariable" as variable_id,
+        "descripcion" as variable_description,
+        "valor" as variable_value,
+        "fecha"::date as variable_at
+
+    from source
+
+)
+
+select * from renamed 
