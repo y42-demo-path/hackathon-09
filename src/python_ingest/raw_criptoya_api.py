535db505dd4353a6886fc24a8609c84a79188a38
Diego Rojas <darb302@gmail.com>
2024-05-27 01:59:48 -0300

    refactor criptoya source code

--- src/python_ingest/raw_criptoya_api.py
+++ src/python_ingest/raw_criptoya_api.py
@@ -20,3 +20,18 @@ def raw_criptoya_usd_ars(context) -> pd.DataFrame:
         logging.error(f"An error occurred. Error status_code: {r.status_code}")
     
     return df
+
+
+@data_loader
+def raw_criptoya_usdt_ars(context) -> pd.DataFrame:
+    url = "https://criptoya.com/api/usdt/ars/100"
+    r = requests.get(url)
+    
+    if r.status_code == 200:
+        data = r.json() 
+        df = pd.DataFrame(data)
+        df = df.T.reset_index()
+    else: 
+        logging.error(f"An error occurred. Error status_code: {r.status_code}")
+    
+    return df
--- src/python_ingest/raw_yahoofinance_api.py
+++ src/python_ingest/raw_yahoofinance_api.py
@@ -6,7 +6,7 @@ import json
 from y42.v1.decorators import data_loader
 
 @data_loader
-def raw_yahoofinance_usd_ars(context) -> pd.DataFrame:
+def raw_ccl_usd_ars(context) -> pd.DataFrame:
     tickers = ['GGAL.BA', 'GGAL', 'YPFD.BA', 'YPF', 'PAMP.BA', 'PAM']
     data = pd.DataFrame(columns=tickers)
     df = pd.DataFrame()
@@ -16,15 +16,11 @@ def raw_yahoofinance_usd_ars(context) -> pd.DataFrame:
         # Select the last available value
         data = data.iloc[[-1]]
 
-    # This line is to interpolate and fill in empty data
-    # It is very useful when using ratios between time series that operate at different hours
-    data.interpolate(method='linear', inplace=True)
-
     # Calculation of the CCL dollar as the average of the CCL of GGAL, PAM, and YPF
-    df['GGAL_CCL'] = data['GGAL.BA'] * 10 / data['GGAL']
-    df['YPF_CCL'] = data['YPFD.BA'] * 1 / data['YPF']
-    df['PAMP_CCL'] = data['PAMP.BA'] * 25 / data['PAM']
-    df['CCL'] = (df['GGAL_CCL'] + df['YPF_CCL'] + df['PAMP_CCL']) / 3
+    df['ggal_ccl'] = data['GGAL.BA'] * 10 / data['GGAL']
+    df['ypf_ccl'] = data['YPFD.BA'] * 1 / data['YPF']
+    df['pamp_ccl'] = data['PAMP.BA'] * 25 / data['PAM']
+    df['ccl_average'] = (df['ggal_ccl'] + df['ypf_ccl'] + df['pamp_ccl']) / 3
     df = df.reset_index()
     
     return df
