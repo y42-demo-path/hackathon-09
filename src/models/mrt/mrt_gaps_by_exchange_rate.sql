49319d33d91846318247e4b08f1f299f6cb8f880
Diego Rojas <darb302@gmail.com>
2024-05-28 20:16:57 -0300

    fix run

--- src/models/mrt/mrt_gaps_by_exchange_rate.sql
+++ src/models/mrt/mrt_gaps_by_exchange_rate.sql
@@ -12,7 +12,7 @@ gaps as (
 
         {{ dbt_utils.safe_divide('total_bid_price', 'official_retailer_dollar') }} -1 as gap_over_official_retailer_exchange_rate,
         {{ dbt_utils.safe_divide('total_bid_price', 'official_wholesale_dollar') }} - 1 as gap_over_official_wholesale_exchange_rate,
-        {{ dbt_utils.safe_divide('total_bid_price', 'avg_mep_rate') }} -1 as gap_over_mep_exchange_rate,
+        {{ dbt_utils.safe_divide('total_bid_price', 'avg_mep_dollar') }} -1 as gap_over_mep_exchange_rate,
 
     from int_unioned_model
     where source_reference not in ('BCRA')
