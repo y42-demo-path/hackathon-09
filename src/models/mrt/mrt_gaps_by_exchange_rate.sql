9ec3b58d16cad72fcfbc916ec0968d2205b302c1
Diego Rojas <darb302@gmail.com>
2024-05-30 00:53:10 -0300

    change materialization to view

--- src/models/mrt/mrt_gaps_by_exchange_rate.sql
+++ src/models/mrt/mrt_gaps_by_exchange_rate.sql
@@ -1,3 +1,6 @@
+{{ config(materialized = 'view') }}
+
+
 with int_unioned_model as ( 
 
     select * from {{ ref('int_exchange_rates_unioned') }}
@@ -12,7 +15,7 @@ gaps as (
 
         {{ dbt_utils.safe_divide('total_bid_price', 'official_retailer_dollar') }} -1 as gap_over_official_retailer_exchange_rate,
         {{ dbt_utils.safe_divide('total_bid_price', 'official_wholesale_dollar') }} - 1 as gap_over_official_wholesale_exchange_rate,
-        {{ dbt_utils.safe_divide('total_bid_price', 'avg_mep_dollar') }} -1 as gap_over_mep_exchange_rate,
+        {{ dbt_utils.safe_divide('total_bid_price', 'avg_mep_dollar') }} -1 as gap_over_mep_exchange_rate
 
     from int_unioned_model
     where source_reference not in ('BCRA')
