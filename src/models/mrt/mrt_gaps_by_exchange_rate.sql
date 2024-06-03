9151a2211fe8a192160ce66ac7fb6800bfe27128
Diego Rojas <darb302@gmail.com>
2024-06-03 19:22:09 -0300

    add threshold for the metrics

--- src/models/mrt/mrt_gaps_by_exchange_rate.sql
+++ src/models/mrt/mrt_gaps_by_exchange_rate.sql
@@ -1,4 +1,8 @@
 {% set gap_over_mep_threshold = 0.05 %}
+{% set gap_over_official_threshold = 0.5 %}
+{% set change_total_bid_threshold = 0.03 %}
+{% set change_gap_over_official_threshold = 0.03 %}
+{% set change_gap_over_mep_threshold = 0.03 %}
 
 
 with int_unioned_model as ( 
@@ -17,12 +21,18 @@ gaps as (
         {{ dbt_utils.safe_divide('total_bid_price', 'official_wholesale_dollar') }} - 1 as gap_over_official_wholesale_exchange_rate,
         {{ dbt_utils.safe_divide('total_bid_price', 'avg_mep_dollar') }} - 1 as gap_over_mep_exchange_rate,
         
+        gap_over_official_retailer_exchange_rate > {{ gap_over_official_threshold }} as is_high_official_gap,
         gap_over_mep_exchange_rate > {{ gap_over_mep_threshold }} as is_high_mep_gap,
 
         lag(total_bid_price) over(
             partition by exchange_rate_name
             order by processed_at
         ) total_bid_price_lagged,
+
+        lag(gap_over_official_retailer_exchange_rate) over(
+            partition by exchange_rate_name
+            order by processed_at
+        ) gap_over_official_exchange_rate_lagged,
 
         lag(gap_over_mep_exchange_rate) over(
             partition by exchange_rate_name
@@ -35,7 +45,7 @@ gaps as (
 
 ),
 
-final as (
+change as (
 
     select 
         
@@ -49,20 +59,35 @@ final as (
         first_value(gap_over_mep_exchange_rate_lagged) over(
             partition by exchange_rate_name
             order by updated_at
-        ) as gap_over_mep_exchange_rate_previous_close_day,
+        ) as gap_over_mep_previous_close_day,
+
+        first_value(gap_over_official_exchange_rate_lagged) over(
+            partition by exchange_rate_name
+            order by updated_at
+        ) as gap_over_official_previous_close_day,
 
 
         {{ dbt_utils.safe_divide(
-            'total_bid_price', 'total_bid_price_previous_close_day'
+            'total_bid_price', 
+            'total_bid_price_previous_close_day'
         ) }} - 1 as change_total_bid_price_previous_day,
 
         {{ dbt_utils.safe_divide(
-            'gap_over_mep_exchange_rate', 'gap_over_mep_exchange_rate_previous_close_day'
-        ) }} - 1 as change_gap_over_mep_previous_day 
+            'gap_over_mep_exchange_rate', 
+            'gap_over_mep_previous_close_day'
+        ) }} - 1 as change_gap_over_mep_previous_day,
 
+        {{ dbt_utils.safe_divide(
+            'gap_over_official_retailer_exchange_rate', 
+            'gap_over_official_previous_close_day'
+        ) }} - 1 as change_gap_over_official_previous_day,
+        
+        change_total_bid_price_previous_day > {{ change_total_bid_threshold }} as is_high_change_bid,
+        change_gap_over_official_previous_day > {{ change_gap_over_official_threshold }} as is_high_change_official_over_mep,
+        change_gap_over_mep_previous_day > {{ change_gap_over_mep_threshold }} as is_high_change_gap_over_mep
 
     from gaps
 
 )
 
-select * from final
+select * from change
