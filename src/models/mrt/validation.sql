96e876819c1d191fabde24258124f656224218fd
Diego Rojas <darb302@gmail.com>
2024-06-06 12:19:20 -0300

    update validation query

--- src/models/mrt/validation.sql
+++ src/models/mrt/validation.sql
@@ -8,6 +8,5 @@ select *
 from ref
 where
 processed_at in (select max(processed_at) from ref)
-and IS_HIGH_CHANGE_TOTAL_BID_PRICE
-ORDER BY gap_over_official_wholesale_exchange_rate desc, processed_at
--- processed_at in (select max(processed_at) from ref)
+and IS_HIGH_MEP_GAP
+ORDER BY processed_at
